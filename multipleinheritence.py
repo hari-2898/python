3048dd10fdb1c83ac4cbbaa417bb3540b35ef530
Hari Govind <govindhari778@gmail.com>
2025-11-07 06:23:00 +0000

    Refactor multiple inheritance example in Python

--- multipleinheritence.py
+++ multipleinheritence.py
@@ -11,7 +11,6 @@ class Student:
         print("Name:", self.name)
         print("Course:", self.course)
 
-
 # Inheritance
 class Test(Student):
     def getMarks(self, marks):
@@ -20,14 +19,12 @@ class Test(Student):
     def displayMarks(self):
         print("Total Marks:", self.marks)
 
-
 class Sports:
     def getSportsMarks(self, spmarks):
         self.spmarks = spmarks
 
     def displaySportsMarks(self):
         print("Sports Marks:", self.spmarks)
-
 
 # Multiple Inheritance
 class Result(Test, Sports):
@@ -43,7 +40,6 @@ class Result(Test, Sports):
             self.grade = "Failed"
         print("Result:", self.grade)
 
-
 # Main Program
 r = int(input("Enter Roll Number: "))
 n = input("Enter Name: ")
@@ -52,7 +48,7 @@ m = int(input("Enter Marks: "))
 s = int(input("Enter Sports Marks: "))
 
 # Creating the object
-print("\n--- Result ---")
+print("Result")
 stud1 = Result()  # instance of child
 stud1.getData(r, n, c)
 stud1.getMarks(m)
@@ -61,3 +57,4 @@ stud1.displayStudent()
 stud1.displayMarks()
 stud1.displaySportsMarks()
 stud1.calculateGrade()
+
