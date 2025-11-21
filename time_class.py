9977e29dd78f6cd06f8bf063fd9aae8f1dc45140
Hari Govind <govindhari778@gmail.com>
2025-11-21 08:54:21 +0000

    Update time_class.py

--- time_class.py
+++ time_class.py
@@ -1,31 +1,35 @@
-
 class Time:
-    def __init__(self, h, m, s):
-        self.__hour = h
-        self.__minute = m
-        self.__second = s
+    def __init__(self, hour=0, minute=0, second=0):
+        self.__hour = hour
+        self.__minute = minute
+        self.__second = second
 
     def __add__(self, other):
-        h = self.__hour + other.__hour
-        m = self.__minute + other.__minute
-        s = self.__second + other.__second
+        if not isinstance(other, Time):
+            raise TypeError("Can only add Time objects")
+        
+        total_seconds = self.__second + other.__second
+        total_minutes = self.__minute + other.__minute
+        total_hours = self.__hour + other.__hour
+        
+        
+        total_minutes += total_seconds 
+        total_seconds %= 60
+        
+        
+        total_hours += total_minutes 
+        total_minutes %= 60
+        
+        return Time(total_hours, total_minutes, total_seconds)
+
+    def __str__(self):
+        return f"{self.__hour:02d}:{self.__minute:02d}:{self.__second:02d}"
 
 
-        if s >= 60:
-            m += s // 60
-            s = s % 60
-        if m >= 60:
-            h += m // 60
-            m = m % 60
-
-        return Time(h, m, s)
-
-    def show(self):
-        print(self.__hour, ":", self.__minute, ":", self.__second)
-
-
-t1 = Time(2, 50, 40)
-t2 = Time(1, 20, 30)
-t3 = t1 + t2
-
-t3.show()
+if __name__ == "__main__":
+    t1 = Time(1, 30, 45)
+    t2 = Time(2, 45, 30)
+    t3 = t1 + t2
+    print(f"Time 1: {t1}")
+    print(f"Time 2: {t2}")
+    print(f"Sum: {t3}")
